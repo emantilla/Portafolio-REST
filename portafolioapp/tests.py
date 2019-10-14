3a098e7c535e27cd2ffcc04b9c6b6c317855fc87
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-10-13 19:08:06 -0500

    Se crea test para listar portafolios (ROJO)

--- portafolioapp/tests.py
+++ portafolioapp/tests.py
@@ -6,4 +6,8 @@ from portafolioapp.models import Usuario, Portafolio
 # Create your tests here.
 
 class PortafolioTestCase(TestCase):
+    def test_list_portafolios(self):
+        url = '/portafolio'
+        response = self.client.get(url, format('json'))
+        self.assertEqual(response.status_code, 200)
 
